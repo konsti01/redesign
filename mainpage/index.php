993aff88549c459086335578ca3010961a19f11c
Sparda <spardasixx@gmail.com>
2017-06-24 07:02:41 +0200

    mainpage

--- mainpage/index.php
+++ mainpage/index.php
@@ -33,11 +33,46 @@
             <!-- RIGHT COL -->
             <div class="row-right">
                 <div class="upside">
-                    <div class="left col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">jobb fent bal</div>
-                    <div class="right col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">jobb fent jobb</div>
+                    <div class="left col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <div class="first">
+                            <img src="img/static.jpg">
+                        </div>
+                        <div class="second">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="third">
+                            <div class="headline">Loren Ipsum</div>
+                            <div class="text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                                anim id est laborum.
+                            </div>
+                            <div class="button">Button</div>
+                        </div>
+                        <div class="fourth">
+                            <div class="headline">Loren Ipsum</div>
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                            <img src="img/static.jpg">
+                        </div>
+                    </div>
+                    <div class="right col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <div class="any">akármi</div>
+                    </div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="downside">jobb lent</div>
+                <div class="downside">lent</div>
             </div>
         </div>
     </div>
