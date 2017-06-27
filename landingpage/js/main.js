14a71901258e9bfa1d2ddaae04189c8d272250cc
Sparda <spardasixx@gmail.com>
2017-06-27 09:55:17 +0200

    owl autoplay

--- landingpage/js/main.js
+++ landingpage/js/main.js
@@ -258,6 +258,9 @@ $(document).ready(function() {
         loop:true,
         margin:10,
         nav:true,
+        autoplay:true,
+        autoplayTimeout:10000,
+        autoplayHoverPause:true,
         responsive:{
             0:{
                 items:1
