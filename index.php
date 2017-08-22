28068e0ad983098a86d9cb8a7725205ef3cc4a96
Sparda <spardasixx@gmail.com>
2017-08-22 16:14:22 +0200

    mainpage media ref

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <a href="/faq">F.A.Q.</a><br>
 <a href="/landingpage">Landing Page</a><br>
 <br>
+<!--
 <a href="/old/mab/index.html">Mérnökadatbázis</a><br>
 <a href="/old/pvsk/index.html">PVSK</a><br>
 <a href="/old/meska/index.html">Meska Default</a><br>
+-->
