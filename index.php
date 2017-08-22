7b1a104f11ef9abcad4dc41292f052ac9fac479f
Sparda <spardasixx@gmail.com>
2017-08-22 14:59:47 +0200

    mainpage media ref

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <a href="/mainpage">Főoldal</a><br>
 <a href="/productlist">Terméklista</a><br>
 <a href="/faq">F.A.Q.</a><br>
-<a href="/landingpage">Landing Page</a>
+<a href="/landingpage">Landing Page</a><br>
+<br>
+<a href="/old/mab/index.html">Mérnökadatbázis</a><br>
+<a href="/old/pvsk/index.html">PVSK</a><br>
+<a href="/old/meska/index.html">Meska Default</a><br>
