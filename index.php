da3863c4d40881d64d724450bc35a6154b7cfd67
Sparda <spardasixx@gmail.com>
2017-08-24 15:59:19 +0200

    mobile fix

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <a href="/mainpage">Főoldal</a><br>
 <a href="/productlist">Terméklista</a><br>
 <a href="/faq">F.A.Q.</a><br>
-<a href="/landingpage">Landing Page</a><br>
-<br>
-
-<a href="/old/mab/index.html">Mérnökadatbázis</a><br>
-<a href="/old/pvsk/index.html">PVSK</a><br>
-<a href="/old/meska/index.html">Meska Default</a><br>
+<a href="/landingpage">Landing Page</a>
