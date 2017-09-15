5c5d6569df1064532d58e3bb7c48248e1a794567
Sparda <spardasixx@gmail.com>
2017-09-15 11:54:58 +0200

    index

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+BOOTSTRAP 3.3.7<br><br>
+
 <a href="/mainpage">Főoldal</a><br>
 <a href="/productlist">Terméklista</a><br>
 <a href="/faq">F.A.Q.</a><br>
-<a href="/landingpage">Landing Page</a>
+<a href="/landingpage">Landing Page</a><br>
+<hr>
+
+BOOTSTRAP 4 BETA<br><br>
+
+<a href="bootstrap4/landingpage">Landing Page</a>
